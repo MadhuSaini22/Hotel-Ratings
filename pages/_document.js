79c89abf41608f6bcfb415e36068af897cac7bda
MadhuSaini22 <madhusaini46810@gmail.com>
2023-04-07 20:32:15 +0530

    updated logo

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/assets/mainlogo.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
